5be775eca2bf0e97b4220f1994bcdc63e5cbf1af
xadamec1 <xadamec1@fi.muni.cz>
2023-11-24 15:18:30 +0100

    upgrade styling of top navbar

---
+++ src/app/_components/NavbarItem.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+const NavbarItem = ({ path, name }: { path: string; name: string }) => (
+	<li className="transition duration-300 hover:bg-default-button">
+		<Link href={path}>{name}</Link>
+	</li>
+);
+
+export default NavbarItem;
--- src/app/camp/[campName]/layout.tsx
+++ src/app/camp/[campName]/layout.tsx
@@ -1,5 +1,11 @@
-import Link from 'next/link';
 import React from 'react';
+import Link from 'next/link';
+
+const NavbarItem = ({ path, name }: { path: string; name: string }) => (
+	<li className="rounded transition duration-300 hover:bg-default-button">
+		<Link href={path}>{name}</Link>
+	</li>
+);
 
 const CampLayout = ({
 	children,
@@ -9,23 +15,13 @@ const CampLayout = ({
 	campName: string;
 }) => (
 	<div>
-		<nav className="bg-default-card">
-			<ul className="container mx-auto flex gap-x-6 px-10 py-4">
-				<li>
-					<Link href="/">Other available camps</Link>
-				</li>
-				<li>
-					<Link href={`/camp/${campName}/about`}>About camp</Link>
-				</li>
-				<li>
-					<Link href={`/camp/${campName}/register`}>Register</Link>
-				</li>
-				<li>
-					<Link href={`/camp/${campName}/news`}>Camp news</Link>
-				</li>
-				<li>
-					<Link href={`/camp/${campName}/contact`}>Contacts</Link>
-				</li>
+		<nav className="my-auto bg-default-card">
+			<ul className=" container mx-auto flex items-center justify-between px-10 py-4">
+				<NavbarItem path="/" name="Other available camps" />
+				<NavbarItem path={`/camp/${campName}/about`} name="About camp" />
+				<NavbarItem path={`/camp/${campName}/register`} name="Register" />
+				<NavbarItem path={`/camp/${campName}/news`} name="Camp news" />
+				<NavbarItem path={`/camp/${campName}/contact`} name="Contacts" />
 			</ul>
 		</nav>
 		{children}
