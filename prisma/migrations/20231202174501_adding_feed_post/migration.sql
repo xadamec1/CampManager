c1b4a06a239d5690ef95c8091f844bc2f0cf139e
xcermak5 <xcermak5@fi.muni.cz>
2023-12-03 09:23:53 +0100

    Adding prisma model and migration

---
+++ prisma/migrations/20231202174501_adding_feed_post/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "FeedPost" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "imagePath" TEXT NOT NULL
+);
