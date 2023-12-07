b4664140243b921195686bd2b7f6ae2670ba6def
Martin Gargalovič <xgargal@fi.muni.cz>
2023-12-07 18:32:58 +0100

    added metadata generation

--- src/app/camp/[campId]/register/page.tsx
+++ src/app/camp/[campId]/register/page.tsx
@@ -1,4 +1,22 @@
 import { CampRegisterForm } from '@/app/_components/CampRegisterForm';
+import { db } from '@/server/db';
+import { Metadata } from 'next';
+
+export async function generateMetadata({
+	params
+}: {
+	params: { campId: string };
+}): Promise<Metadata> {
+	const camp = await db.camp.findUnique({
+		where: {
+			id: +params.campId
+		}
+	});
+	return {
+		title: `Registration:${camp?.name}`,
+		description: `registration for camp ${camp?.name}`
+	};
+}
 
 const registerPage = () => <CampRegisterForm />;
 
