172381166a6702124c0a019fb23ffafaea55807a
xadamec1 <xadamec1@fi.muni.cz>
2023-12-08 15:44:45 +0100

    ignore eslint

--- next.config.js
+++ next.config.js
@@ -1,17 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "*.googleusercontent.com",
-          port: "",
-          pathname: "**",
-        },
-      ],
-    },
-  };
-  
-module.exports = nextConfig
+	reactStrictMode: true,
+	eslint: {
+		// Warning: This allows production builds to successfully complete even if
+		// your project has ESLint errors.
+		ignoreDuringBuilds: true
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: '*.googleusercontent.com',
+				port: '',
+				pathname: '**'
+			}
+		]
+	}
+};
 
+module.exports = nextConfig;
