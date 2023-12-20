c9e56941e21b7b3665178ad4e5b8de19d79b39c1
xcermak5 <xcermak5@fi.muni.cz>
2023-12-20 16:15:42 +0100

    Adding andrej to admins

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -25,7 +25,7 @@ const LoginStatus = () => {
 			</div>
 		);
 	}
-	const admins = ['petr.adamec@skaut.cz', '12marting02@gmail.com'];
+	const admins = ['petr.adamec@skaut.cz', '12marting02@gmail.com', 'andrej.cermak.1999@gmail.com'];
 
 	if (admins.includes(data?.user.email as string)) {
 		redirect('/admin/center');
